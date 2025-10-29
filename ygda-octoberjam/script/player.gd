93c7d19fa4890eaf885ddd96be2a7c45ef9f98a0
simplyorange8 <128482382+simplyorange8@users.noreply.github.com>
2025-10-28 22:34:08 -0400

    dash
    woohoo implemented dash
    you can dash once eveyr time you leave the floor
    cooldown of 0.5s

--- ygda-octoberjam/script/player.gd
+++ ygda-octoberjam/script/player.gd
@@ -1,10 +1,15 @@
 extends CharacterBody2D
 
-const SPEED: int = 200
+var SPEED: int = 200
 const JUMP_POW: int = -550
 var x_direction: int = 0
 var max_health: int = 100
 var currHealth: int = 100
+
+#stuff for dash
+var canDash: bool = true
+var dashCD: bool = false
+var dashing: bool = false
 
 signal death
 
@@ -22,17 +27,40 @@ func health_Update() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		if not dashing:
+			velocity += get_gravity() * delta
+		else:
+			velocity.y = 0
+	
+	if is_on_floor():
+		canDash = true
 	
 	if Input.is_action_pressed("Up") and is_on_floor():
 		velocity.y += JUMP_POW
+		
+	#this should work even when you dont have power up, just like make canDash always false if you dont have it (so that you cant change direction while dashign)
+	if not dashing:
+		if Input.is_action_pressed("Left"):
+			x_direction = -1
+		elif Input.is_action_pressed("Right"):
+			x_direction = 1
+		else:
+			x_direction = 0
 	
-	if Input.is_action_pressed("Left"):
-		x_direction = -1
-	elif Input.is_action_pressed("Right"):
-		x_direction = 1
+	
+	#dash/attack
+	if Input.is_action_pressed("Shoot"):
+		if(not dashCD) and canDash:
+			$dashtimer.start()
+			$dashCooldown.start()
+			dashing = true
+			dashCD = true
+			canDash = false
+		
+	if dashing:
+		SPEED = 800
 	else:
-		x_direction = 0
+		SPEED = 200
 	
 	velocity.x = x_direction * SPEED
 	
@@ -45,3 +73,12 @@ func die():
 func dmg(num: int):
 	currHealth -= num
 	health_Update()
+
+
+#dash stuff
+func _on_dashCooldown_timeout() -> void:
+	dashCD = false
+
+func _on_dashtimer_timeout() -> void:
+	dashing = false
+	pass # Replace with function body.
