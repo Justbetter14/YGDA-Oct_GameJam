c1ad7f406ed971292fc94f7c7ff910eeb4aff483
Justbetter14 <145606090+Justbetter14@users.noreply.github.com>
2025-11-01 17:46:00 -0400

    Added a bite cooldown to meele

--- ygda-octoberjam/enemy/meleeBasic/enemy_melee.gd
+++ ygda-octoberjam/enemy/meleeBasic/enemy_melee.gd
@@ -6,6 +6,9 @@ var x_direction: int = 1
 var dmg: int = 10
 var player: CharacterBody2D = null
 
+var canBite: bool = true
+var shouldMove: bool = true
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	player = get_parent().get_node("Player")
@@ -14,15 +17,31 @@ func _ready():
 func _physics_process(delta: float) -> void:
 	if hp <= 0:
 		queue_free()
-	
-	var direction = (player.global_position - global_position).normalized()
-	
-	velocity = SPEED * direction
-	move_and_slide()
+	if shouldMove == true:
+		var direction = (player.global_position - global_position).normalized()
+		
+		velocity = SPEED * direction
+		move_and_slide()
+	else:
+		if canBite:
+			bite()
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
-		body.dmg(dmg)
+		shouldMove = false
+		if canBite:
+			bite()
 
 func takeDmg(num: int):
 	hp -= num
+
+func bite():
+	player.dmg(dmg)
+	
+func _on_attack_cooldown_timeout() -> void:
+	$"Bullet Cooldown".stop()
+	canBite = true
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		shouldMove = true
