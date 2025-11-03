377c89bb4058fd813de2f03ce6a7baa7d24de371
Justbetter14 <145606090+Justbetter14@users.noreply.github.com>
2025-11-02 21:01:28 -0500

    Fireball 360

--- ygda-octoberjam/player/fireball.gd
+++ ygda-octoberjam/player/fireball.gd
@@ -2,21 +2,22 @@ extends AnimatedSprite2D
 
 var speed: int = 225
 var dmg: int = 100
-
-var x_direction: int = 1
+var direction: Vector2 = Vector2.RIGHT
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	direction = direction.normalized()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float):
-	position.x += speed * x_direction * delta
+	position += speed * direction * delta
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()
 
-func _on_aoe_body_entered(body: Node2D) -> void:
+func _on_aoe_body_entered(body: Node2D) -> void:	
 	if body.is_in_group("enemy"):
+		print("enemyHit")
 		body.takeDmg(10) # Destroy the enemy
 		queue_free()
+	
