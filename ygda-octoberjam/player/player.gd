f85a2470c1f2e604b2422cbb525190a6a6304f99
Justbetter14 <145606090+Justbetter14@users.noreply.github.com>
2025-11-02 19:27:30 -0500

    Added Wall Jump and Reorganized Physics

--- ygda-octoberjam/player/player.gd
+++ ygda-octoberjam/player/player.gd
@@ -6,16 +6,22 @@ var x_direction: int = 0
 var max_health: int = 100
 var currHealth: int = 110
 @export var healthbar : ProgressBar;
-#stuff for dash
-var canDash: bool = true
-var dashCD: bool = false
-var dashing: bool = false
 var iframe: bool = false
 var dir: String = 'right'
 var knockback: Vector2 = Vector2.ZERO
 var knockback_timer: float = 0.0
+
+# Dash
+var canDash: bool = true
+var dashCD: bool = false
+var dashing: bool = false
+
+# Double Jump
 var canDoubleJump: bool = true
 var jumpCount: int = 0
+
+# Wall Jump
+var canWallJump: bool = true
 
 signal death
 
@@ -35,22 +41,90 @@ func health_Update() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	if not is_on_floor():
-		if not dashing:
-			velocity += get_gravity() * delta
-		else:
-			velocity.y = 0
+	gravityCooldown(delta)
 	
-	if is_on_floor():
-		canDash = true
-		jumpCount = 0
+	animation()
 	
+	doubleJump()
+	dash()
+	wallJump()
+	
+	knockBack(delta)
+	
+	move_and_slide()
+
+func die():
+	death.emit()
+
+func dmg(num: int):
+	print('triggered')
+	if iframe == false:
+		currHealth -= num
+		health_Update()
+		iframe = true
+		$iFrame.start()
+
+func _on_dash_timer_timeout() -> void:
+	dashing = false
+	pass # Replace with function body.
+
+func _on_dash_cooldown_timeout() -> void:
+	dashCD = false
+	pass # Replace with function body.
+
+func _on_i_frame_timeout() -> void:
+	iframe = false
+	pass # Replace with function body.
+
+func applyKnockback(direction: Vector2, force: float, knockbackDuration: float, dmg: int) -> void:
+	print(iframe)
+	if iframe == false:
+		print('yay')
+		knockback = direction*force
+		knockback_timer = knockbackDuration
+		dmg(dmg)
+
+func doubleJump():
 	if Input.is_action_just_pressed("Up") and (is_on_floor() or (canDoubleJump == true and jumpCount < 2)):
 		print(jumpCount)
 		jumpCount += 1
-		velocity.y += JUMP_POW
+		velocity.y = JUMP_POW
+
+func wallJump():
+	if Input.is_action_just_pressed("Up") and is_on_wall() and canWallJump == true:
+			if Input.is_action_pressed("Right"):
+				print("wall jumped right")
+				velocity.y = JUMP_POW
+				velocity.x = -100
+			if Input.is_action_pressed("Left"):
+				print("wall jumped left")
+				velocity.y = JUMP_POW
+				velocity.x = 100
+
+func dash():
+	if Input.is_action_pressed("Dash"):
+		if(not dashCD) and canDash:
+			$dashTimer.start()
+			$dashCooldown.start()
+			dashing = true
+			dashCD = true
+			canDash = false
 		
-	#this should work even when you dont have power up, just like make canDash always false if you dont have it (so that you cant change direction while dashign)
+	if dashing:
+		SPEED = 550
+	else:
+		SPEED = 200
+
+func knockBack(delta: float):
+	if knockback_timer > 0.0:
+		velocity = knockback
+		knockback_timer -= delta
+		if knockback_timer <= 0.0:
+			knockback = Vector2.ZERO
+	else:
+		velocity.x = x_direction * SPEED
+
+func animation():
 	if not dashing:
 		if Input.is_action_pressed("Left"):
 			x_direction = -1
@@ -72,63 +146,14 @@ func _physics_process(delta: float) -> void:
 			$Sprite2D.play('dashLeft')
 		if(dir == 'right'):
 			$Sprite2D.play('dashRight')
+
+func gravityCooldown(delta: float):
+	if not is_on_floor():
+		if not dashing:
+			velocity += get_gravity() * delta
+		else:
+			velocity.y = 0
 	
-	#dash/attack
-	if Input.is_action_pressed("Dash"):
-		if(not dashCD) and canDash:
-			$dashTimer.start()
-			$dashCooldown.start()
-			dashing = true
-			dashCD = true
-			canDash = false
-		
-	if dashing:
-		SPEED = 550
-	else:
-		SPEED = 200
-	
-	if knockback_timer > 0.0:
-		velocity = knockback
-		knockback_timer -= delta
-		if knockback_timer <= 0.0:
-			knockback = Vector2.ZERO
-	else:
-		velocity.x = x_direction * SPEED
-	
-	move_and_slide()
-
-func die():
-	death.emit()
-
-func dmg(num: int):
-	print('triggered')
-	if iframe == false:
-		currHealth -= num
-		health_Update()
-		iframe = true
-		$iFrame.start()
-
-#dash stuffwd
-
-func _on_dash_timer_timeout() -> void:
-	dashing = false
-	pass # Replace with function body.
-
-
-func _on_dash_cooldown_timeout() -> void:
-	dashCD = false
-	pass # Replace with function body.
-
-
-func _on_i_frame_timeout() -> void:
-	iframe = false
-	pass # Replace with function body.
-	
-func applyKnockback(direction: Vector2, force: float, knockbackDuration: float, dmg: int) -> void:
-	print(iframe)
-	if iframe == false:
-		print('yay')
-		knockback = direction*force
-		knockback_timer = knockbackDuration
-		dmg(dmg)
-	
+	if is_on_floor():
+		canDash = true
+		jumpCount = 0
