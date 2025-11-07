9df8c66cd2aa5a7a1c1a655a4b36a476c8648658
simplyorange8 <128482382+simplyorange8@users.noreply.github.com>
2025-11-07 15:59:28 -0500

    level 3 (empty)

---
+++ ygda-octoberjam/main/Levels/3.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+var basicEnemy = preload("res://enemy/meleeBasic/enemy_melee.tscn")
+
+func _ready() -> void:
+	var melee1 = basicEnemy.instantiate()
+	melee1.global_position = Vector2(-518.0, -413.0)
+	melee1.cardChance = 100
+	melee1.cardsList = [load("res://Cards/Resources/walljumpCard.tres")]
+	add_child(melee1)
+
+
+
+func _on_player_death() -> void:
+	get_parent().death()
+	pass # Replace with function body.
+
+
+func _on_level_3_end_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		get_parent().nextLevel()
+		queue_free()
+	pass # Replace with function body.
