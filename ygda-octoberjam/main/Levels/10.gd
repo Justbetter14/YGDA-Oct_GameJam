b018293ed71eb1df7540722635051aca64406632
Justbetter14 <145606090+Justbetter14@users.noreply.github.com>
2025-11-07 23:06:48 -0500

    Added remaining level shells and fixed a enemy bug

--- ygda-octoberjam/main/Levels/10.gd
+++ ygda-octoberjam/main/Levels/10.gd
@@ -13,11 +13,13 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	$Checkpoint.play("default")
 
-func _on_level_4_end_body_entered(body: Node2D) -> void:
-	if body.is_in_group("player"):
-		get_parent().nextLevel()
-		queue_free()
 
 func _on_player_death() -> void:
 	get_parent().death()
 	pass # Replace with function body.
+
+
+func _on_level_10_end_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		get_parent().nextLevel()
+		queue_free()
