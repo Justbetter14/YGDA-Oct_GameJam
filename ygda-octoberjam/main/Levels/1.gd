134027bd2f9ca0c2d91db380ffe44d1c4b41238e
Justbetter14 <145606090+Justbetter14@users.noreply.github.com>
2025-11-07 21:27:01 -0500

    Added Tilesets & Started up to 8 Levels & Added checkpoints

--- ygda-octoberjam/main/Levels/1.gd
+++ ygda-octoberjam/main/Levels/1.gd
@@ -1,13 +1,10 @@
 extends Node2D
 
-var basicEnemy = preload("res://enemy/meleeBasic/enemy_melee.tscn")
-
 func _ready() -> void:
-	var melee1 = basicEnemy.instantiate()
-	melee1.global_position = Vector2(-518.0, -413.0)
-	melee1.cardChance = 100
-	melee1.cardsList = [load("res://Cards/Resources/swordCard.tres")]
-	add_child(melee1)
+	pass
+
+func _process(delta: float) -> void:
+	$Checkpoint.play("default")
 
 func _on_level_1_end_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
