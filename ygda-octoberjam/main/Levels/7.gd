743c67307006404cf0068958559dc3e87431c8c6
Justbetter14 <145606090+Justbetter14@users.noreply.github.com>
2025-11-09 16:30:03 -0500

    Revert "yeah"

--- ygda-octoberjam/main/Levels/7.gd
+++ ygda-octoberjam/main/Levels/7.gd
@@ -1,20 +1,23 @@
 extends Node2D
 
-var cardSlot = preload("res://Cards/Resources/doublejumpCard.tres")
+var basicEnemy = preload("res://enemy/meleeBasic/enemy_melee.tscn")
 
 func _ready() -> void:
 	$Checkpoint.global_position = $Player.global_position + Vector2(0, -7)
-	$CanvasLayer/Inventory/GridContainer/cardButton1.icon = cardSlot.texture
+	var melee1 = basicEnemy.instantiate()
+	melee1.global_position = Vector2(-518.0, -413.0)
+	melee1.cardChance = 33
+	melee1.cardsList = []
+	add_child(melee1)
 
 func _process(delta: float) -> void:
 	$Checkpoint.play("default")
 
-func _on_player_death() -> void:
-	get_parent().death()
-	pass # Replace with function body.
-
-
-func _on_level_11_end_body_entered(body: Node2D) -> void:
+func _on_level_7_end_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		get_parent().nextLevel()
 		queue_free()
+
+func _on_player_death() -> void:
+	get_parent().death()
+	pass # Replace with function body.
--- ygda-octoberjam/main/Levels/9.gd
+++ ygda-octoberjam/main/Levels/9.gd
@@ -1,29 +1,14 @@
 extends Node2D
 
 var basicEnemy = preload("res://enemy/meleeBasic/enemy_melee.tscn")
-var basicExplode = preload("res://enemy/destructEnemy/EnemyExplode.tscn")
-var basicRange = preload("res://enemy/rangedBasic/enemy_range.tscn")
 
 func _ready() -> void:
 	$Checkpoint.global_position = $Player.global_position + Vector2(0, -7)
-	var explode1 = basicExplode.instantiate()
-	var explode2 = basicExplode.instantiate()
-	var explode3 = basicExplode.instantiate()
-	var explode4 = basicExplode.instantiate()
-	var explode5 = basicExplode.instantiate()
-	var explode6 = basicExplode.instantiate()
-	explode1.global_position = Vector2(1167, -760)
-	explode2.global_position = Vector2(1333, -840)
-	explode3.global_position = Vector2(1500, -1000)
-	explode4.global_position = Vector2(-200, -400)
-	explode5.global_position = Vector2(400, -840)
-	explode6.global_position = Vector2(-300, -850)
-	add_child(explode1)
-	add_child(explode2)
-	add_child(explode3)
-	add_child(explode4)
-	add_child(explode5)
-	add_child(explode6)
+	var melee1 = basicEnemy.instantiate()
+	melee1.global_position = Vector2(-518.0, -413.0)
+	melee1.cardChance = 33
+	melee1.cardsList = []
+	add_child(melee1)
 
 func _process(delta: float) -> void:
 	$Checkpoint.play("default")
@@ -32,7 +17,8 @@ func _on_player_death() -> void:
 	get_parent().death()
 	pass # Replace with function body.
 
-func _on_level_13_end_body_entered(body: Node2D) -> void:
+
+func _on_level_9_end_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		get_parent().nextLevel()
 		queue_free()
